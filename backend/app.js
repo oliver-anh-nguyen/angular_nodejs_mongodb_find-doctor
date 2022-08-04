23619a2560ae32af7956e7124f7f54decfd85236
Henry Ha <106703135+henryhacode@users.noreply.github.com>
2022-08-04 16:41:10 -0500

    Initialize backend structure
    Create basic directories and files for backend.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('combined'));
 
 app.use('/', (req, res, next) => {
     res.json('Hello world');
-})
+});
 
-app.listen(3000, () => {console.log('listen on port 3000')})
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({ error: err });
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
