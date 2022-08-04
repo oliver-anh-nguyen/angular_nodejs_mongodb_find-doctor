9b87a069fcd85b50f09565ff7a0e0240b30352cc
Henry Ha <106703135+henryhacode@users.noreply.github.com>
2022-08-04 16:58:55 -0500

    be: use .env config
    use .env config for PORT and others

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
